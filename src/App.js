86dda8b2e80962d269fe38e4283d8e7953fb5fad
Carol Silva <contatocarolinebs@gmail.com>
2022-08-17 09:55:43 -0300

    Iniciando meu repositório react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+const titulo = <h1> Testandoaqui</h1>
 function App() {
+  const objeto = {
+    nome:  'Carol'
+  }
+  const ativo = false;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {titulo}
+   <p className= {ativo ? 'azul' : 'amarelo'}> { objeto.nome} </p>
+   </>
   );
 }
 
