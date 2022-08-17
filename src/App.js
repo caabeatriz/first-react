decd1180a7b55fe76fe91555edb06afdccd33f94
Carol Silva <contatocarolinebs@gmail.com>
2022-08-17 19:47:53 -0300

    minha solução

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
 
-const titulo = <h1> Testandoaqui</h1>
-function App() {
-  const objeto = {
-    nome:  'Carol'
+// Mostre os dados da aplicação, como aprensetado no vídeo
+// Não utilize CSS externo, use o style para mudar as cores
+// Se a situação estiver ativa pinte de verde, inativa vermelho
+// Se o gasto for maior que 10000 mostre uma mensagem
+const luana = {
+  cliente: 'Luana',
+  idade: 27,
+  compras: [
+    { nome: 'Notebook', preco: 'R$ 2500' },
+    { nome: 'Geladeira', preco: 'R$ 3000' },
+    { nome: 'Smartphone', preco: 'R$ 1500' },
+  ],
+  ativa: true,
+};
+
+const mario = {
+  cliente: 'Mario',
+  idade: 31,
+  compras: [
+    { nome: 'Notebook', preco: 'R$ 2500' },
+    { nome: 'Geladeira', preco: 'R$ 3000' },
+    { nome: 'Smartphone', preco: 'R$ 1500' },
+    { nome: 'Guitarra', preco: 'R$ 3500' },
+  ],
+  ativa: false,
+};
+
+const App = () => {
+  const dados = luana;
+
+  function somarGastos(compras){
+    let result = 0;
+    compras.forEach(element => {
+      let strg = Number(element.preco.match(/(\d+)/)[0]);
+      result = result + strg
+     
+    });
+    return result;
   }
 
-  function mostrarNome(sobrenome){
-    return `Caról ${sobrenome}`
+  function ativaParagrafo(){
+    if(somarGastos(dados.compras) > 10000){
+      return true;
+    }
   }
-  const ativo = false;
-  return (
-    <>
-    {mostrarNome('Sìlva')}
-   <p className= {ativo ? 'azul' : 'amarelo'}> { objeto.nome} </p>
-   </>
-  );
-}
+  somarGastos(dados.compras)
+  return <div>
+    <p> Nome: {dados.cliente}</p>
+    <p> Idade: {dados.idade}</p>
+    <p> Situação: <label className= { dados.ativa ? 'ativa' : 'desativado'}>{dados.ativa ? 'ativa' : 'desativado'} </label></p>
+    <p> Total gasto: {somarGastos(dados.compras)}</p>
+    <p className={ativaParagrafo() ? 'enable' : 'disabled'}> O total excede o limite </p>
+  </div>;
+};
 
 export default App;
